f7e0169506c4bf99ab15017b1942dcdca38c2da1
Fernando Costa <ferndcosta353@gmail.com>
2023-08-29 18:36:37 -0300

    feat: add final game result messages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 let deckId
+let computerScoreCount = 0
+let myScoreCount = 0
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
 const remainingCardsEL = document.getElementById("remaining-cards")
+const computerScore = document.getElementById("computer-score")
+const myScore = document.getElementById("my-score")
+const header = document.getElementById("header")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -30,6 +35,15 @@ drawCardBtn.addEventListener("click", () => {
             remainingCardsEL.textContent = `Remaining cards: ${data.remaining}`
             if (data.remaining === 0) {
                 drawCardBtn.disabled = true
+                if (computerScoreCount > myScoreCount) {
+                    header.textContent = "The computer won the game!"
+                }
+                else if (computerScoreCount < myScoreCount) {
+                    header.textContent = "You won the game!"
+                }
+                else {
+                    header.textContent = "It's a tie game!"
+                }
             }
         })
 })
@@ -41,9 +55,13 @@ function determineCardWinner(card1, card2) {
     const card2ValueIndex = valueOptions.indexOf(card2.value)
     
     if (card1ValueIndex > card2ValueIndex) {
-        return "Card 1 wins!"
+        computerScoreCount++
+        computerScore.textContent = `Computer Score: ${computerScoreCount}`
+        return "The Computer Wins!"
     } else if (card1ValueIndex < card2ValueIndex) {
-        return "Card 2 wins!"
+        myScoreCount++
+        myScore.textContent = `My Score: ${myScoreCount}`
+        return "You won!"
     } else {
         return "War!"
     }
